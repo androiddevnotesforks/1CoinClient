e069ee2f3f068b4c64eed7062dacd6128d3741b2
grafgad <grafgad@mail.ru>
2022-08-30 23:49:53 +0300

    JetPack Compose + Compose Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id ("com.android.application")
-    id ("org.jetbrains.kotlin.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -20,7 +20,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            proguardFiles( getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -30,13 +33,40 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
 }
 
 dependencies {
+
+    val composeVersion = "1.2.1"
+    //Jetpack Compose
+    implementation("androidx.compose.ui:ui:${composeVersion}")
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.5.1")
+    // Navigation for Compose
+    implementation("androidx.navigation:navigation-compose:2.5.1")
+    // Animations
+    implementation("androidx.compose.animation:animation:${composeVersion}")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:${composeVersion}")
+    // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:${composeVersion}")
+    // Material Design
+    implementation("androidx.compose.material:material:${composeVersion}")
+    // Material design icons
+    implementation("androidx.compose.material:material-icons-core:${composeVersion}")
+    // Integration with observables
+    implementation("androidx.compose.runtime:runtime-livedata:${composeVersion}")
+
     //Chucker
-    val chukerVresion = "3.5.2"
-    debugImplementation("com.github.chuckerteam.chucker:library:${chukerVresion}")
-    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:${chukerVresion}")
+    val chukerVersion = "3.5.2"
+    debugImplementation("com.github.chuckerteam.chucker:library:${chukerVersion}")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:${chukerVersion}")
 
     //Core
     implementation("androidx.core:core-ktx:1.8.0")
@@ -53,4 +83,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // UI Tests
+    // androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
 }
