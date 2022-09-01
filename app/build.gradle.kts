15f8f91e5d2195b9dad882a194f738c13f981cc7
Bittner322 <sandreyf1972@gmail.com>
2022-09-01 22:00:50 +0600

    FT-7: Добавил LeakCanary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
 
 dependencies {
 
+    // LeakCanary
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
+
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("com.google.android.material:material:1.6.1")
