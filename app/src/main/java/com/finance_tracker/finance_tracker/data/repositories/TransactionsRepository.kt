ea02044f50f2394726af764d80e3ab779069cbcf
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-10-11 23:28:25 +0300

    FT-38: Edit transactions list - UI

--- app/src/main/java/com/finance_tracker/finance_tracker/data/repositories/TransactionsRepository.kt
+++ app/src/main/java/com/finance_tracker/finance_tracker/data/repositories/TransactionsRepository.kt
@@ -1,9 +1,11 @@
 package com.finance_tracker.finance_tracker.data.repositories
 
 import androidx.compose.ui.graphics.Color
+import com.finance_tracker.finance_tracker.core.common.DateFormatType
 import com.finance_tracker.finance_tracker.domain.models.Account
 import com.finance_tracker.finance_tracker.domain.models.Category
 import com.finance_tracker.finance_tracker.domain.models.Transaction
+import com.finance_tracker.finance_tracker.domain.models.TransactionType
 import com.financetracker.financetracker.TransactionsEntityQueries
 import org.koin.core.annotation.Factory
 import java.util.Date
@@ -12,6 +14,7 @@ import java.util.Date
 class TransactionsRepository(
     private val transactionsEntityQueries: TransactionsEntityQueries
 ) {
+
     fun getAllTransactions(): List<Transaction> {
         return transactionsEntityQueries.getAllFullTransactions {
                 id, type, amount, amountCurrency, categoryId,
@@ -35,8 +38,18 @@ class TransactionsRepository(
                     )
                 },
                 amount = amount,
-                date = Date(date)
+                date = date
             )
         }.executeAsList()
+    }
+
+    fun getTotalTransactionAmountByDateAndType(
+        date: Date,
+        type: TransactionType
+    ): Double {
+        return transactionsEntityQueries.getTotalTransactionAmountByDateAndType(
+            date = DateFormatType.CommonDateFormat.format(date),
+            type = type
+        ).executeAsOneOrNull()?.SUM ?: 0.0
     }
 }
