3ba074c841d5104a5b95839c05edd39ea2624fad
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-10-24 19:54:34 +0300

    FT-57: Fix getting ViewModel for Desktop

--- common/src/desktopMain/kotlin/com/finance_tracker/finance_tracker/core/common/ViewModelDi.kt
+++ common/src/desktopMain/kotlin/com/finance_tracker/finance_tracker/core/common/ViewModelDi.kt
@@ -1,9 +1,15 @@
 package com.finance_tracker.finance_tracker.core.common
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import org.koin.core.context.GlobalContext
 
 @Composable
 actual inline fun <reified T : ViewModel> getViewModel(): T {
-    return GlobalContext.get().get()
+    val viewModel by remember {
+        mutableStateOf<T>(GlobalContext.get().get())
+    }
+    return viewModel
 }
