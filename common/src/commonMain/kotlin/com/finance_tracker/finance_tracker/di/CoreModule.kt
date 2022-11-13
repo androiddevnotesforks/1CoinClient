a7b144abcc503c648d115cb76c0070143647314b
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-11-13 14:21:34 +0300

    FT-73: Add Ktor logging

--- common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/di/CoreModule.kt
+++ common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/di/CoreModule.kt
@@ -1,5 +1,6 @@
 package com.finance_tracker.finance_tracker.di
 
+import io.github.aakira.napier.Napier
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
@@ -30,6 +31,11 @@ private fun provideHttpClient(jsonFactory: Json): HttpClient {
             json(jsonFactory)
         }
         install(Logging) {
+            logger = object: Logger {
+                override fun log(message: String) {
+                    Napier.v(message, tag = "HTTP-Client")
+                }
+            }
             level = LogLevel.ALL
         }
     }
