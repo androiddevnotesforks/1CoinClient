5741377014cac636d08b7db0b75fd2619557e5b5
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-11-14 22:45:16 +0300

    FT-83: Prepare SQLDelight for future migrations

--- common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/data/database/mappers/CategoriesEntityMapper.kt
+++ common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/data/database/mappers/CategoriesEntityMapper.kt
@@ -1,7 +1,7 @@
 package com.finance_tracker.finance_tracker.data.database.mappers
 
 import com.finance_tracker.finance_tracker.domain.models.Category
-import com.financetracker.financetracker.CategoriesEntity
+import com.financetracker.financetracker.data.CategoriesEntity
 
 fun CategoriesEntity.categoryToDomainModel(): Category {
     return Category(
