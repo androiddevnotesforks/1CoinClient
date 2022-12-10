a1844a741804e31e3ae0d46ce1e745dddec34361
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2022-12-10 18:45:39 +0300

    Remove println

--- common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/presentation/common/formatters/DoubleFormatter.kt
+++ common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/presentation/common/formatters/DoubleFormatter.kt
@@ -5,11 +5,9 @@ import com.finance_tracker.finance_tracker.core.common.NumberFormatters
 private val formatter = NumberFormatters.DoubleFormatter()
 
 fun Double.format(): String {
-    println("format: $this - ${formatter.format(this)}")
     return formatter.format(this)
 }
 
 fun String.parse(): Double? {
-    println("parse: $this - ${formatter.parse(this)}")
     return formatter.parse(this)
 }
