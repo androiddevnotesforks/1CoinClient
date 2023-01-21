742a58d398fde39e87c986f0d7e0ad5ebaffc537
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2023-01-21 12:44:37 +0300

    FT-179: Add support of AppDatabase.db in Distribution desktop builds

--- common/src/desktopMain/kotlin/com/finance_tracker/finance_tracker/data/database/DriverFactory.kt
+++ common/src/desktopMain/kotlin/com/finance_tracker/finance_tracker/data/database/DriverFactory.kt
@@ -1,13 +1,45 @@
 package com.finance_tracker.finance_tracker.data.database
 
 import com.finance_tracker.finance_tracker.AppDatabase
+import com.finance_tracker.finance_tracker.core.common.OS
+import com.finance_tracker.finance_tracker.core.common.OSProvider
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import java.io.File
 
 actual class DriverFactory {
+
+  private val appDataPath by lazy {
+    val appName = "1Coin"
+    when (OSProvider.os) {
+      OS.Windows -> "${System.getProperty("user.home")}\\AppData\\Local\\$appName"
+      OS.Mac -> "/Users/${System.getProperty("user.name")}/Library/Application Support/$appName"
+      OS.Unix -> ""
+      OS.Solaris -> ""
+    }
+  }
+
+  private val separator by lazy {
+    if (OSProvider.os == OS.Windows) {
+      "\\"
+    } else {
+      "/"
+    }
+  }
+
   actual fun createDriver(): SqlDriver {
-    return JdbcSqliteDriver("jdbc:sqlite:AppDatabase.db").also {
-      AppDatabase.Schema.create(it)
+    File(appDataPath).createIfNotExists()
+    val filePath = "$appDataPath${separator}AppDatabase.db"
+    return JdbcSqliteDriver("jdbc:sqlite:$filePath").also {
+      if (!File(filePath).exists()) {
+        AppDatabase.Schema.create(it)
+      }
+    }
+  }
+
+  private fun File.createIfNotExists() {
+    if (!exists()) {
+      mkdirs()
     }
   }
 }
