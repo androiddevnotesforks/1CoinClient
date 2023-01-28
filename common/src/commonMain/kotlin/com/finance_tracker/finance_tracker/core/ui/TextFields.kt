1e9359fa085507eb9779a281ee1319235ca90332
Bittner322 <sandreyf1972@gmail.com>
2023-01-28 23:28:52 +0600

    [FT-195] Replaced standard textfield to OTP textfield

--- common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/core/ui/TextFields.kt
+++ common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/core/ui/TextFields.kt
@@ -205,7 +205,7 @@ private fun ClickableOutlinedTextField(
 }
 
 @Composable
-fun ConfirmationCodeTextField(
+fun CoinConfirmationCodeTextField(
     otpText: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
