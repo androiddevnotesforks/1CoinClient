9142299dbb2f890b23abde948e6f438c92c62b51
Vitaly Peryatin <peryatin.vitalik37@gmail.com>
2023-02-04 13:09:47 +0300

    Increase code and version name

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.finance_tracker.finance_tracker"
         minSdk = 26
         targetSdk = 33
-        versionCode = 6
+        versionCode = 7
         versionName = rootProject.extra["appVersion"] as String
         signingConfig = signingConfigs.getByName("debug")
         val appName = rootProject.extra["appName"] as String
