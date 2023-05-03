fd6bb95cb04e97fe985f6e33f1bec2d3b1ae4d4b
maltsev-gorskij <55731252+maltsev-gorskij@users.noreply.github.com>
2023-05-03 17:42:20 +0300

    [FT-222] Implemented theme change animation (#75)
    - Implemented theme change animation
    - Bumped Odyssey version to latest stable

--- common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/data/repositories/ThemeRepository.kt
+++ common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/data/repositories/ThemeRepository.kt
@@ -3,16 +3,12 @@ package com.finance_tracker.finance_tracker.data.repositories
 import com.finance_tracker.finance_tracker.data.settings.ThemeSettings
 import com.finance_tracker.finance_tracker.domain.models.ThemeMode
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
 class ThemeRepository(
     private val themeSettings: ThemeSettings
 ) {
-
-    fun getThemeModeFlow(): Flow<ThemeMode?> {
-        return themeSettings.getThemeMode()
-    }
+    fun getThemeMode() = themeSettings.themeMode
 
     suspend fun setThemeMode(themeMode: ThemeMode) {
         withContext(Dispatchers.Default) {
